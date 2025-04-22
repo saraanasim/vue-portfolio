d7eb23481f2dc6b8905c00e5faa416be47f49f38
saraanasim <saraanofficial@gmail.com>
2025-04-22 07:35:32 +0500

    build(app): prod test 6

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/vue-portfolio/" : "/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/vue-portfolio/",
   plugins: [
     vue(),
     vueJsx(),
