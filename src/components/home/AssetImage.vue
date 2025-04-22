6a1c5cfb203a945881b88db9e1add5dc84d2bace
saraanasim <saraanofficial@gmail.com>
2025-04-22 06:29:44 +0500

    fix(ui): fixed asset image

--- src/components/home/AssetImage.vue
+++ src/components/home/AssetImage.vue
@@ -4,12 +4,12 @@
         <div class="absolute inset-0 flex bg-black bg-opacity-50">
             <div class="w-full mx-4 my-2 border-2 border-dimmest-text grid grid-cols-2 text-white-text font-bold">
                 <div class="col-span-1 flex flex-col justify-start items-start pt-2 pl-2 ">
-                    <p>/FULL_STACK_DEV</p>
-                    <p class="font-extrabold text-lt-blue-hl">SARAAN ASIM</p>
+                    <p class="text-xs sm:text-sm md:text-base">/FULL_STACK_DEV</p>
+                    <p class="font-extrabold text-lt-blue-hl text-sm sm:text-base md:text-lg">SARAAN ASIM</p>
                 </div>
                 <div class="col-span-1 flex justify-end pt-2 pr-2">
                     <div class="flex flex-col items-end gap-2">
-                        <p>V:1 (V1.1)</p>
+                        <p class="text-xs sm:text-sm md:text-base">V:1 (V1.1)</p>
                         <!-- <div
                             class="flex flex-col bg-gray-bg border-2 border-t-black-bg border-l-black-bg border-r-dim-text border-b-dim-text py-2 px-4">
                             <p>Rerouting</p>
@@ -19,12 +19,12 @@
                     </div>
                 </div>
                 <div class="col-span-1 flex justify-start items-end pb-2 pl-2">
-                    <p>DS455DXGFV75454DFFD</p>
+                    <p class="text-xs sm:text-sm md:text-base">DS455DXGFV75454DFFD</p>
                 </div>
                 <div class="col-span-1 flex justify-end items-end pb-2 pr-2">
-                    <div class="flex gap-4">
-                        <p>EXT_CODE_003_TEST_RAW</p>
-                        <p>ATC 14:33:28:10</p>
+                    <div class="flex gap-2 md:gap-4 flex-wrap justify-end">
+                        <p class="text-xs sm:text-sm md:text-base">EXT_CODE_003_TEST_RAW</p>
+                        <p class="text-xs sm:text-sm md:text-base">ATC 14:33:28:10</p>
                     </div>
                 </div>
             </div>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,8 +8,10 @@
         <div class="w-full h-full flex ">
             <div class="h-full w-4 border-y-2 border-l-2 border-dimmest-text"></div>
             <div class="w-full grid grid-cols-3 gap-2">
-                <div class="col-span-3 lg:col-span-2 flex flex-col justify-center border-t-4 border-green-hl">
+                <div
+                    class="col-span-3 lg:col-span-2 flex flex-col justify-center border-t-4 border-green-hl relative overflow-hidden">
                     <AssetImage imageSrc="/src/assets/images/saraan-1.png" />
+                    <div class="cyber-scanline"></div>
                 </div>
                 <div class="col-span-3 lg:col-span-1 overflow-y-auto max-h-screen lg:max-h-full">
                     <div class="grid grid-cols-2 gap-2">
@@ -70,3 +72,32 @@ export default {
     mixins: [lifecycleLoggerMixin],
 };
 </script>
+
+<style scoped>
+.cyber-scanline {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background-color: rgba(0, 229, 255, 0.5);
+    opacity: 0;
+    box-shadow: 0 0 5px rgba(0, 229, 255, 0.8);
+    z-index: 2;
+    animation: scan 1.5s linear infinite;
+}
+
+.col-span-3:hover .cyber-scanline {
+    opacity: 1;
+}
+
+@keyframes scan {
+    0% {
+        top: 0;
+    }
+
+    100% {
+        top: 100%;
+    }
+}
+</style>
