2719f9eeb3d17159ba01c5523c337cf33573720d
saraanasim <saraanofficial@gmail.com>
2025-05-18 09:49:27 +0500

    build(app): changed base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-portfolio/",
+  base: "/",
   // base: "/",
   plugins: [
     vue(),
